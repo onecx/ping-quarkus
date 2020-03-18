ed640511ea84dadafe5a1ed1449cab3900136069
Andrej Petras <andrej.petras@capgemini.com>
2020-03-18 15:02:04 +0100

    Add the tests

--- src/main/java/org/tkit/quarkus/hello/rs/HelloQuarkusRestController.java
+++ src/main/java/org/tkit/quarkus/hello/rs/HelloQuarkusRestController.java
@@ -1,14 +1,19 @@
 package org.tkit.quarkus.hello.rs;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 @Path("hello")
+@Consumes(MediaType.APPLICATION_JSON)
 public class HelloQuarkusRestController {
 
     @GET
+    @Produces(MediaType.APPLICATION_JSON)
     public Response hello() {
-        return Response.ok("Hello oquarkus").build();
+        return Response.ok("Hello quarkus").build();
     }
 }
--- src/test/java/org/tkit/quarkus/hello/rs/HelloQuarkusRestControllerT.java
+++ src/test/java/org/tkit/quarkus/hello/rs/HelloQuarkusRestControllerT.java
@@ -1,13 +1,28 @@
 package org.tkit.quarkus.hello.rs;
 
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.tkit.quarkus.hello.test.AbstractTest;
+
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 public class HelloQuarkusRestControllerT  extends AbstractTest {
 
     @Test
     public void dummyTest() {
-        System.out.println("This is not test");
+        String tmp = given()
+                .when()
+                .contentType(MediaType.APPLICATION_JSON)
+                .get("/hello")
+                .then()
+                .statusCode(Response.Status.OK.getStatusCode())
+                .extract().body().asString();
+
+        Assertions.assertEquals("Hello quarkus", tmp);
     }
 }
